0e0d2eeddee176f88bfaa498c1f73a7f2a1f65ac
levin li <3259963@qq.com>
2021-10-17 21:56:44 +0800

    应用 fp-ts 包

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,35 @@
 import { DataFrame, read_csv } from 'danfojs';
-import { either, taskEither } from 'fp-ts';
+import { fold as eitherFold } from 'fp-ts/Either';
+import { none, Option, some } from 'fp-ts/Option';
+import { TaskEither, tryCatch } from 'fp-ts/TaskEither';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useState } from 'react';
 import ChooseFile from '../components/ChooseFile';
-import Table from '../components/Table';
+import ShowDf from '../components/ShowDf';
 import styles from '../styles/Home.module.css';
 
-const readCsv = (url: string): taskEither.TaskEither<Error, DataFrame> =>
-  taskEither.tryCatch(
+const createDfFromUrl = (url: string): TaskEither<Error, DataFrame> =>
+  tryCatch(
     () => read_csv(url, {}),
     (err) => new Error(String(err))
   );
 
 const Home: NextPage = () => {
-  const [columns, setColumns] = useState<Array<string>>([]);
-  const [data, setData] = useState<Array<Array<string>>>([[]]);
+  const [df, setDf] = useState<Option<DataFrame>>(none);
 
   const handleInputFileChange: React.ChangeEventHandler<HTMLInputElement> = (
     e
   ) => {
     const file = e.target.files?.[0];
     const url = URL.createObjectURL(file);
-    readCsv(url)().then(
-      either.fold(
+    createDfFromUrl(url)().then(
+      eitherFold(
         // TODO: 读取文件失败, 打印错误到控制台. 未来需要修改该方法在界面提醒用户操作失败
         console.error,
         // 读取文件成功
         (df) => {
-          setColumns(df.columns);
-          setData(df.values as Array<Array<string>>);
+          setDf(some(df));
         }
       )
     );
@@ -50,7 +50,7 @@ const Home: NextPage = () => {
             handleInputFileChange={handleInputFileChange}
           />
         </div>
-        <Table columns={columns} data={data} />
+        <ShowDf df={df} />
       </main>
     </div>
   );
