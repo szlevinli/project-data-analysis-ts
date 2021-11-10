fa439e9764017c04ba329b34f74aecbae03faed7
levin li <3259963@qq.com>
2021-11-10 23:17:28 +0800

    refacte

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,38 @@
 import Box from '@mui/material/Box';
-import { DataFrame, read_csv } from 'danfojs';
+import { DataFrame } from 'danfojs';
 import { pipe } from 'fp-ts/function';
-import { fromNullable, map as optionMap, none, Option } from 'fp-ts/Option';
-import { TaskEither, tryCatch } from 'fp-ts/TaskEither';
+import { none, Option, some, fold as optionFold } from 'fp-ts/Option';
+import { fold as eitherFold } from 'fp-ts/Either';
 import type { NextPage } from 'next';
-import React, { useState } from 'react';
+import { always, compose } from 'ramda';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import ChooseFile from '../components/ChooseFile';
 import ShowDf from '../containers/ShowDf';
-
-const createDfFromUrl = (url: string): TaskEither<Error, DataFrame> =>
-  tryCatch(
-    () => read_csv(url, {}),
-    (err) => new Error(String(err))
-  );
+import { useDfFromEvent } from '../libs/hooks/useDf';
 
 const Home: NextPage = () => {
-  const [df, setDf] = useState<Option<TaskEither<Error, DataFrame>>>(none);
+  const [df, setDf] = useState<Option<DataFrame>>(none);
+  const [changeEvent, setChangeEvent] =
+    useState<Option<ChangeEvent<HTMLInputElement>>>(none);
+  const optionEitherDf = useDfFromEvent(changeEvent);
 
-  const handleInputFileChange: React.ChangeEventHandler<HTMLInputElement> = (
-    e
-  ) =>
-    pipe(
-      e.target.files?.[0],
-      fromNullable,
-      optionMap(URL.createObjectURL),
-      optionMap(createDfFromUrl),
-      setDf
+  useEffect(() => {
+    // TODO: use stack snake
+    const onLeftHandleError = console.log;
+    const onRightSetDf = compose<DataFrame, Option<DataFrame>, void>(
+      setDf,
+      some
     );
+    const onNoneSetDfToNone = compose(setDf, always(none));
+    const onSomeSetDfToSomeDf = eitherFold<any, DataFrame, void>(
+      onLeftHandleError,
+      onRightSetDf
+    );
+    pipe(optionEitherDf, optionFold(onNoneSetDfToNone, onSomeSetDfToSomeDf));
+  }, [optionEitherDf]);
+
+  const handleInputFileChange: React.ChangeEventHandler<HTMLInputElement> =
+    compose(setChangeEvent, some);
 
   return (
     <Box>
